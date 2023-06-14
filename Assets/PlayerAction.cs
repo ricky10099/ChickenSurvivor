cff423c5d3209a7a481aff78ecf2e6161b06c08c
ricky_10099 <rickyto10099@gmail.com>
2023-06-15 01:09:23 +0900

    Player prefab adjustment

--- Assets/PlayerAction.cs
+++ Assets/PlayerAction.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class PlayerAction : MonoBehaviour
 {
-    [SerializeField] float AccelBias = 2.0f; //iPadの傾き係数
-    [SerializeField] float RotateBias = 3.0f; //回転係数
+    [SerializeField] float AccelBias = 2.0f;
+    [SerializeField] float RotateBias = 3.0f; 
 
     // Start is called before the first frame update
     void Start()
@@ -31,15 +31,11 @@ public class PlayerAction : MonoBehaviour
         }
 
         Vector3 Dir = new Vector3(h, 0, v);
-        //入力方向を向く
-        transform.forward = Vector3.Slerp(transform.forward, //現在の向き
-                                            Dir, //本来、到着したい向き
-                                            Time.deltaTime * RotateBias); //その差分を0.0～１.0で指定
+        transform.forward = Vector3.Slerp(transform.forward, Dir, Time.deltaTime * RotateBias); 
 
+        // if(v > 0)
+        // transform.position += transform.forward * AccelBias;
         Vector3 Dir2 = new Vector3(h, -1, v);
-        Physics.gravity = 9.81f * Dir2.normalized; //重力方向を変位
-        //transform.position = Vector3.Lerp(Pos, //現在のプレイヤーの位置
-        //new Vector3(MoveBall.transform.position.x, Pos.y, Pos.z), //行きたいボールの位置
-        //Time.deltaTime * MoveBias); //マイルドに動く係数
+        Physics.gravity = 9.81f * Dir2.normalized;
     }
 }
